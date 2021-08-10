e93e2f154e0e5ed8b2a50eaf66eecc928ff59a81
Patrick <patnavarre@gmail.com>
2021-08-10 15:43:01 -0400

    updated application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input'
+import {
+    FormControl,
+    FormHelperText,
+    InputLabel,
+    Button,
+    CircularProgress,
+    Snackbar,
+    Grid,
+  } from "@material-ui/core";
+  
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,138 +26,56 @@ export default function Application() {
   const classes = useStyles();
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <div>
-        <TextField required id="standard-required" label="Required" defaultValue="Hello World" />
-        <TextField disabled id="standard-disabled" label="Disabled" defaultValue="Hello World" />
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="standard-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="standard-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="standard-search" label="Search field" type="search" />
-        <TextField
-          id="standard-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField id="filled-search" label="Search field" type="search" variant="filled" />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="outlined"
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="outlined"
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
-        <TextField id="outlined-search" label="Search field" type="search" variant="outlined" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="outlined"
-        />
-      </div>
-    </form>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: "30vh" }}
+    >
+      <Grid item xs={12}>
+        <form
+          className={classes.root}
+          autoComplete="on"
+        >
+          <br />
+          <br />
+            <InputLabel htmlFor="component-firstName">First Name</InputLabel>
+            <Input
+              id="component-firstName"
+              name="First Name"
+            />
+            <InputLabel htmlFor="component-lastName">Last Name</InputLabel>
+            <Input
+              id="component-lastName"
+              name="Last Name"
+            />
+            <InputLabel htmlFor="component-date-of-birth">Date of Birth</InputLabel>
+            <Input
+              id="component-date-of-birth"
+              name="date-of-birth"
+            />
+
+          <br />
+            <InputLabel htmlFor="component-city">City</InputLabel>
+            <Input
+              id="component-city"
+              name="city"
+            />
+             <InputLabel htmlFor="component-email">Email</InputLabel>
+            <Input
+              id="component-email"
+              name="email"
+            />
+             <InputLabel htmlFor="component-city">Phone Number</InputLabel>
+
+          <br />
+          <Button variant="contained" color="primary" type="submit" >
+            Submit
+          </Button>
+        </form>
+      </Grid>
+    </Grid>
   );
 }
